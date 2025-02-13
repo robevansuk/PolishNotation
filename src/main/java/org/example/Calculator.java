1d4c47bf32b1e86d8d9f1659b09c651507568508
Rob Evans <robevansuk@noreply.users.github.io>
2025-02-13 11:35:21 +0000

    Possible Working solution

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Calculator implements CalculatorInterface {
+
+    public static final String DELIMITER = " ";
+    private List<String> buffer;
+
+    public Calculator() {
+        this.buffer = new LinkedList();
+    }
+
+    // 4 5 +
+    // 5 4 -
+    // 6 3 *
+    // 5 4 - 6 *
+    @Override
+    public Long calculate(String expression) {
+        Arrays.stream(expression.split(DELIMITER)).map(String::trim).forEach(buffer::add);
+
+        int i = 2;
+        while (buffer.size() != 1 && i < buffer.size()) {
+            if (!buffer.get(i).matches("\\+")
+                && !buffer.get(i).matches("-")
+                && !buffer.get(i).matches("\\*")
+                && !buffer.get(i).matches("/")) {
+                i++;
+                continue;
+            }
+            runCalc(i);
+            i--;
+        }
+
+        return Long.parseLong(buffer.get(0));
+    }
+
+    private void runCalc(int i) {
+        String operator = buffer.get(i);
+        Long num1 = Long.parseLong(buffer.get(i -2));
+        Long num2 = Long.parseLong(buffer.get(i -1));
+
+        switch (operator) {
+            case "+":
+                buffer.set(i -2, String.valueOf(num1 + num2));
+                break;
+            case "-":
+                buffer.set(i -2, String.valueOf(num1 - num2));
+                break;
+            case "*":
+                buffer.set(i -2, String.valueOf(num1 * num2));
+                break;
+            case "/":
+                buffer.set(i -2, String.valueOf(num1 / num2));
+                break;
+        }
+        buffer.remove(i);
+        buffer.remove(i -1);
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,35 @@
+import org.example.Calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator testObject;
+
+    @BeforeEach
+    void setUp() {
+        testObject  = new Calculator();
+    }
+
+    @Test
+    void shouldCalculateResultOfInstruction() {
+        assertEquals(9l, testObject.calculate("5 4 +"));
+    }
+
+    // 3 (enter) 4 − 5 + -> 3 4 - 5 + = 3 - 4 + 5
+    // 3 − 4 + 5 -> 3 4 - 5 +
+    // 3 4 + 5 6 + ×
+    // 7 11 x
+    @Test
+    void shouldCalculateResultOfMoreThanOneInstruction() {
+        assertEquals(4l, testObject.calculate("3 4 - 5 +")); // 3 - 4 + 5 = 4
+    }
+
+    @Test
+    void shouldCalculateResultOfMoreThanOneInstructionWithMultiplication() {
+        assertEquals(77l, testObject.calculate("3 4 + 5 6 + *")); // 3 - 4 + 5 = 4
+    }
+}
